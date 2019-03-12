f3cdba1003d5573e8bd9518a5317d778f0f88241
Tony KINO <tony.kino.3@gmail.com>
2019-03-12 23:32:33 +0100

    Jobs table spec

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -17,5 +17,33 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    build(:job)
+  end
+
+  describe 'is creatable' do
+    let(:job) { create(:job) }
+
+    it { expect(job.id).not_to be_nil }
+    it { expect(job.job).not_to be_nil }
+  end
+
+  describe 'with_workforces' do
+    let(:job) { create(:job, :with_workforces) }
+
+    it { expect(job.id).not_to be_nil }
+    it { expect(job.job).not_to be_nil }
+    it { expect(job.men_workforces).not_to be_nil }
+  end
+
+  describe 'Database' do
+    it { is_expected.to have_db_column(:id).of_type(:integer) }
+    it { is_expected.to have_db_column(:year).of_type(:string) }
+    it { is_expected.to have_db_column(:contract_type).of_type(:string) }
+    it { is_expected.to have_db_column(:job).of_type(:string) }
+    it { is_expected.to have_db_column(:men_workforces).of_type(:integer) }
+    it { is_expected.to have_db_column(:women_workforces).of_type(:integer) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  end
 end

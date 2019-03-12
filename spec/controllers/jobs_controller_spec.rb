6b01b35b0f324c61c3e926a7f1a0bf676323a6ad
Tony KINO <tony.kino.3@gmail.com>
2019-03-13 00:55:59 +0100

    JobsController#index spec

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -3,5 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe JobsController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 end

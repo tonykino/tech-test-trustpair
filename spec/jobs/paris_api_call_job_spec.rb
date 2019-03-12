41b3b3011eb9475b3f3f91fc1f5a8f41ebcd3046
Tony KINO <tony.kino.3@gmail.com>
2019-03-12 22:09:55 +0100

    Fix job name spec

--- spec/jobs/paris_api_call_job_spec.rb
+++ spec/jobs/paris_api_call_job_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe JobsImporterJob, type: :job do
+RSpec.describe ParisApiCallJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
